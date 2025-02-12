b21c1c3c06cf9d558a317c070d46abcbd5a7d779
Jason3859 <augustinej548@gmail.com>
2025-02-12 16:00:39 +0530

    changes in rock paper scissors

--- src/main/kotlin/main/project/rockpaperscissors/MainGame.kt
+++ src/main/kotlin/main/project/rockpaperscissors/MainGame.kt
@@ -10,11 +10,19 @@ fun rockPaperScissors() {
     println("Game Over!")
     println("Final Score -> You: $playerPoints, Computer: $computerPoints")
 
-    if (playerPoints > computerPoints) {
-        println("Congratulations, you won the game!")
-    } else if (playerPoints < computerPoints) {
-        println("I won the game! Better luck next time.")
-    } else {
-        println("It's a tie!")
-    }
+    // if (playerPoints > computerPoints) {
+    //     println("Congratulations, you won the game!")
+    // } else if (playerPoints < computerPoints) {
+    //     println("I won the game! Better luck next time.")
+    // } else {
+    //     println("It's a tie!")
+    // }
+
+    println(
+        when {
+            playerPoints > computerPoints -> "Congratulations, You won the game!"
+            playerPoints < computerPoints -> "Better luck next time. I won"
+            else -> "Tie"
+        }
+    )
 }
