7e578ca587875621873abdd0cbc42b04ca9a522d
Jason3859 <augustinej548@gmail.com>
2025-02-25 12:36:34 +0530

    Update Game.kt

--- src/main/kotlin/main/project/rockpaperscissors/Game.kt
+++ src/main/kotlin/main/project/rockpaperscissors/Game.kt
@@ -1,7 +1,7 @@
 package main.project.rockpaperscissors
 
 fun game() {
-    println("Rock, Paper, Scissors? (Type your choice)")
+    println("Rock, Paper, Scissors (Type your choice)")
     val playerChoice = readln().lowercase()
     val computerChoice = computerChoice().lowercase()
 
